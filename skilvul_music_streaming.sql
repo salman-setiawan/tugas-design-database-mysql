b3c08f2bb0eeea0f02f05dafbd5bfaeaaaf718cc
Salman Setiawan <11191066@student.itk.ac.id>
2021-12-07 22:24:57 +0800

    Update skilvul_music_streaming.sql

--- skilvul_music_streaming.sql
+++ skilvul_music_streaming.sql
@@ -1,23 +1,29 @@
+-- Create Database --
+create database skilvulmusic;
+
+-- Go to Database --
+use skilvulmusic;
+
 -- Create Table User --
 create table `user` (
   `user_id` int not null auto_increment primary key,
   `name` varchar(50) not null default '',
   `email` varchar(50) not null default '',
   `password` varchar(50) not null default ''
-)
+);
 
 -- Create Table Singer --
 create table `singer` (
   `user_id` int not null auto_increment primary key,
   `name` varchar(50) not null default ''
-)
+);
 
 -- Create Table Album --
 create table `album` (
   `album_id` int(10) null,
   `name` varchar(50) not null default '' collate 'utf8_general_ci',
   primary key ('album_id') using btree
-)
+);
 
 -- Create Table Track --
 create table `user` (
@@ -27,7 +33,7 @@ create table `user` (
   `album` varchar(50) not null,
   constraint `album` foreign key (`album`) references `music_player`.`album` (`album_id`) on update cascade on delete cascade,
   constraint `singer` foreign key (`singer`) references `music_player`.`singer` (singer_id`) on update cascade on delete cascade
-)
+);
 
 -- Create Table Playlist --
 create table `playlist` (
@@ -35,7 +41,7 @@ create table `playlist` (
   `name` varchar(50) not null default collate 'utf8_general_ci',
   `user_id` varchar(50) not null,
   constraint `user` foreign key (`user_id`) references `user` (`user_id`) on update cascade on delete cascade
-)
+);
 
 -- Create Table Playlist_Track --
 create table `playlist_track` (
@@ -43,4 +49,4 @@ create table `playlist_track` (
   `track_id` varchar(50) not null default collate 'utf8_general_ci',
   constraint `playlist` foreign key (`playlist_id`) references `music_player` (`playlist_id`) on update cascade on delete cascade,
   constraint `track` foreign key (`track_id`) references `track` (track_id`) on update cascade on delete cascade
-)
+);
